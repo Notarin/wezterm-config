2eb62a52d8a5ae3992ebaad42b789cf2e2a056ae
Notarin <424c414e4b@gmail.com>
2024-12-21 04:49:59 -0500

    Prettied up the tab bar

--- .config/wezterm/decorations.lua
+++ .config/wezterm/decorations.lua
@@ -1,12 +1,22 @@
 local wezterm = require 'wezterm'
 
-return function (config)
-    config.enable_tab_bar = false
+return function(config)
+    config.window_decorations = "NONE"
+    config.enable_tab_bar = true
+    config.use_fancy_tab_bar = false
+    config.hide_tab_bar_if_only_one_tab = true
+    config.show_close_tab_button_in_tabs = false
+    config.show_tab_index_in_tab_bar = false
+    config.show_new_tab_button_in_tab_bar = false
     config.window_background_opacity = 1.0
     config.window_padding = {
         left = '0cell',
         right = '0cell',
         top = '0cell',
         bottom = '0cell',
+    }
+    config.window_frame = {
+        font = wezterm.font { family = 'Firacode Nerd Font', weight = 'Bold' },
+        font_size = 12.0,
     }
 end
--- .config/wezterm/theme.lua
+++ .config/wezterm/theme.lua
@@ -2,6 +2,19 @@ local wezterm = require 'wezterm'
 
 return function(config)
     local rose_pine = wezterm.color.get_builtin_schemes()['rose-pine']
+    print(wezterm.color.get_builtin_schemes()['rose-pine'])
     rose_pine.selection_bg = '#6e6a86'
+    rose_pine.tab_bar = {
+        background = '#191724',
+        active_tab = {
+          bg_color = '#324',
+          fg_color = '#fff',
+          intensity = 'Bold',
+        },
+        inactive_tab = {
+          bg_color = '#213',
+          fg_color = '#888',
+        },
+    }
     config.colors = rose_pine
 end
