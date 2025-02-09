6c84c751db1e682ddfa87d5ddbcfe2570edd8eda
Notarin <424c414e4b@gmail.com>
2025-02-09 00:25:14 -0500

    Removed everything not wezterm.
    This repo now only tracks wezterm config.

--- .config/nvim/lua/packages/FloatingTerminal.lua
+++
@@ -1,9 +0,0 @@
-return {
-	{
-		"numToStr/FTerm.nvim",
-		opts = {
-			height = 0.9,
-			width = 0.9,
-		},
-	},
-}
--- .config/nvim/lua/packages/gitsigns.lua
+++
@@ -1,9 +0,0 @@
-return {
--- 	'lewis6991/gitsigns.nvim', -- shows git added/removed lines to the left of the line numbers
--- 	dependencies = {
--- 		'nvim-lua/plenary.nvim'
--- 	},
--- 	config = function()
--- 		require('gitsigns').setup({})
--- 	end
-}
